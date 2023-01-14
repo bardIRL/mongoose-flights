73c9709b4a640e7267a6a68335aef118d6037bd5
bardIRL <115611931+bardIRL@users.noreply.github.com>
2023-01-13 23:43:20 -0800

    user's new flight now shows on the list of booked flights.

--- controllers/flights.js
+++ controllers/flights.js
@@ -3,6 +3,7 @@ const Flight = require("../models/flight");
 module.exports = {
   index,
   new: newFlight,
+  create,
 };
 
 function index(req, res) {
@@ -13,4 +14,14 @@ function index(req, res) {
 
 function newFlight(req, res) {
   res.render("flights/new");
+}
+
+function create(req, res) {
+  const flight = new Flight(req.body);
+  flight.save(function (err) {
+    if (err) console.log(err);
+    if (err) return res.redirect("/flights/new");
+    console.log(flight);
+    res.redirect("/flights");
+  });
 }
--- models/flight.js
+++ models/flight.js
@@ -1,29 +1,39 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const flightSchema = new Schema({
-  airline: {
-    type: String,
-    enum: ["American", "Southwest", "United", "Delta"],
+const flightSchema = new Schema(
+  {
+    airline: {
+      type: String,
+      required: true,
+      enum: ["American", "Southwest", "United", "Delta"],
+    },
+    airport: {
+      type: String,
+      required: true,
+      default: "SFO",
+      enum: ["DFW", "SFO", "LAX", "DEN", "JFK", "CLT", "SJC"],
+    },
+    flightNo: {
+      type: Number,
+      required: true,
+      min: 10,
+      max: 9999,
+    },
+    departs: {
+      type: Date,
+      required: true,
+      default: new Date(new Date().setFullYear(new Date().getFullYear() + 1)),
+    },
+    seats: {
+      type: Number,
+      required: true,
+      enum: [1, 2, 3, 4],
+    },
   },
-  airport: {
-    type: String,
-    default: "SFO",
-    enum: ["DFW", "SFO", "LAX", "DEN", "JFK", "CLT", "SJC"],
-  },
-  flightNo: {
-    type: Number,
-    min: 10,
-    max: 9999,
-  },
-  departs: {
-    type: Date,
-    default: new Date(new Date().setFullYear(new Date().getFullYear() + 1)),
-  },
-  seats: {
-    type: Number,
-    default: 1,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = mongoose.model("Flight", flightSchema);
